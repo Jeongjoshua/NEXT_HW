6ac9b4bc61d4ed2ee09ba020458848e75ef4c911
HonggyuLee-02 <eltair500@gmail.com>
2024-06-08 13:21:24 +0900

    basic

---
+++ MZgame/Ourgame/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return
+
+def name(request):
+    return
+
+def game(request):
+    return
+
+def gameover(request):
+    return
